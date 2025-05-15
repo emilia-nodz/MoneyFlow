bd38113f98631e884575ecee5d0f9bea96bb7566
emilia-nodz <extra.maslo.game@gmail.com>
2025-05-15 21:36:59 +0200

    Expenses list without colors working

--- frontend/src/app/components/expenses-list/expenses-list.component.ts
+++ frontend/src/app/components/expenses-list/expenses-list.component.ts
@@ -4,16 +4,18 @@ import { Expense } from '../../models/expense';
 import { CommonModule } from '@angular/common';
 import { Category } from '../../models/category';
 import { CategoryService } from '../../services/category.service';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-expenses-list',
-  imports: [CommonModule],
+  imports: [CommonModule, MatTabsModule],
   templateUrl: './expenses-list.component.html',
   styleUrl: './expenses-list.component.css'
 })
 export class ExpensesListComponent{
   expenses: Expense[] = [];
   categories: Category[] = [];
+  category_name: String = "";
 
   constructor(
     private expensesService: ExpenseService,
@@ -22,13 +24,24 @@ export class ExpensesListComponent{
     this.expensesService.getAllExpenses().subscribe(data => {
       this.expenses = data;
     });
-
-
-    // TO-DO: now it gets all categories, make it so that, only each expenses category is shown
+    
     this.categoryService.getAllCategories().subscribe(data => {
       this.categories = data;
     });
   }
    
+  currentExpenseCategory(expense: Expense): boolean {
+    if (!expense.category) {
+      return false;
+    }
+
+    let found = this.categories.find(cat => String(cat.id) === String(expense.category!.id));
+    if (found) {
+      let name = found.name;
+      this.category_name = name[0].toUpperCase() + name.slice(1);
+    }
+
+    return true;
+  }
 
 }
